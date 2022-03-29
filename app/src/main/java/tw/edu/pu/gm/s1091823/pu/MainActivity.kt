e09623c49a7c4b89a4fad64b9631b8a299bce8c3
L <ades97111@gmail.com>
2022-03-29 22:28:27 +0800

    文字修改->靜宜之美

---
+++ app/src/main/java/tw/edu/pu/gm/s1091823/pu/MainActivity.kt
@@ -0,0 +1,26 @@
+package tw.edu.pu.gm.s1091823.pu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MotionEvent
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        if (txv.text == "靜宜之美"){
+            txv.text = "螢幕觸控"
+        }
+        else{
+            txv.text = "靜宜之美"
+        }
+        return true
+    }
+
+
+
+}
